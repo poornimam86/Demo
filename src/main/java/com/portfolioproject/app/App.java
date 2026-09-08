980ba2e4d2ca769a3c9f5454ba1245c393b3b093
Poornima <poornimameduri@kluniversity.in>
2026-09-08 12:07:54 +0530

    Updated Project

--- src/main/java/com/portfolioproject/app/App.java
+++ src/main/java/com/portfolioproject/app/App.java
@@ -3,8 +3,12 @@ package com.portfolioproject.app;
 /**
  * Hello world!
  */
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        System.out.println("=========================");
+        System.out.println("Portfolio Management Project");
+        System.out.println("============================");
     }
 }
